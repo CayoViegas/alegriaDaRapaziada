4abc82eeb4fbc8814838f11987f4e66480795205
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2020-10-20 23:14:30 -0300

    Create OddEvenSort.py

---
+++ OddEvenSort.py
@@ -0,0 +1,16 @@
+def oddEvenSort(arr, n): 
+    isSorted = 0
+    while isSorted == 0: 
+        isSorted = 1
+        temp = 0
+        for i in range(1, n-1, 2): 
+            if arr[i] > arr[i+1]: 
+                arr[i], arr[i+1] = arr[i+1], arr[i] 
+                isSorted = 0
+
+        for i in range(0, n-1, 2): 
+            if arr[i] > arr[i+1]: 
+                arr[i], arr[i+1] = arr[i+1], arr[i] 
+                isSorted = 0
+
+    return arr
