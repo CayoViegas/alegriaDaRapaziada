e4fe59811707ef1dcabd15b7a44c35fa282b5887
Edimar Chaves Jr <edimar.junior@ccc.ufcg.edu.br>
2021-10-30 10:50:08 -0300

    Added Statitics Scripts

--- median_mean_max_min_24h.R
+++ median_mean_max_min_24h.R
@@ -3,21 +3,21 @@ require(dplyr)
 library(lubridate)
 library(ggplot2)
 
-data <- read.csv("out.csv")
+date <- read.csv("out.csv")
 base = 14400
-hour_4=data$begin_stamp[1] + base
-data <- data %>% mutate(date = ymd_hms(as.POSIXct(begin_stamp, origin="1970-01-01")))
+hour_four=date$begin_stamp[1] + base
+date <- date %>% mutate(date = ymd_hms(as.POSIXct(begin_stamp, origin="1970-01-01")))
 
-data <- data %>% mutate(hour = trunc((window-1)/240)) 
+date <- date %>% mutate(hour = trunc((window-1)/240)) 
 
-data <- data %>% mutate(begin_stamp=(((hour_4+(base*hour))-begin_stamp)/60))
+date <- date %>% mutate(begin_stamp=(((hour_four+(base*hour))-begin_stamp)/60))
 
-data <- data %>% mutate(median=median*1000)
-data <- data %>% mutate(max=max*1000)
-data <- data %>% mutate(min=min*1000)
-data <- data %>% mutate(mean=mean*1000)
+date <- date %>% mutate(median = median * 10 * 10 * 10)
+date <- date %>% mutate(max = max * 10 * 10 * 10)
+date <- date %>% mutate(min = min * 10 * 10 * 10)
+date <- date %>% mutate(mean = mean * 10 * 10 * 10)
 
-plot <- data %>% ggplot() + geom_point(aes(begin_stamp, log(median), color="median"), alpha = 0.5) +
+plot <- date %>% ggplot() + geom_point(aes(begin_stamp, log(median), color="median"), alpha = 0.5) +
                                    geom_point(aes(begin_stamp, log(max), color="max" ), alpha = 0.5) +
                                    geom_point(aes(begin_stamp, log(min), color="min"), alpha = 0.5) +
                                    geom_point(aes(begin_stamp, log(mean), color="mean"), alpha = 0.5) +
